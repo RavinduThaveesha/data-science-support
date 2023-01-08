6bcd5c859b72bd2a276a4e4a1e5c8723e9145c8e
Ravindu Thaveesha <thaveesha.ravindu2@gmail.com>
2023-01-08 21:22:47 +0800

    add video game total earn pred. data load

---
+++ building-deep-learning-applications-tensorFlow/video_game_total_eran_prediction.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+
+# load training data set from CSV file
+training_data_df = pd.read_csv('sales_data_training.csv', dtype=float)
+
+# load testing data set from CSV file
+test_data_df = pd.read_csv('sales_data_test.csv')
+
+# pull out columns for X (data to train with) and Y (value to predict)
+X_training = training_data_df.drop('total_earnings', axis=1).values
+Y_training = training_data_df[['total_earnings']].values
+X_testing = test_data_df.drop('total_earnings', axis=1).values
+Y_testing = test_data_df[['total_earnings']].values
+
+# all data needs to be scaled to a small range like 0 to 1 for the neural
+# network to work well. Create scalers for the inputs and outputs.
+X_scaler = MinMaxScaler(feature_range=(0, 1))
+Y_scaler = MinMaxScaler(feature_range=(0, 1))
+
+# scale both the training inputs and outputs
+X_scaled_training = X_scaler.fit_transform(X_training)
+Y_scaled_training = Y_scaler.fit_transform(Y_training)
+
+# it's very important that the training and test data are scaled with the same scaler.
+X_scaled_testing = X_scaler.transform(X_testing)
+Y_scaled_testing = Y_scaler.transform(Y_testing)
+
+# print(X_training.shape)
+# print(X_scaled_testing.shape)
+# print(Y_scaled_testing.shape)
+
+# print("Y values were scaled by multiplying by {:.10f} and adding {:.4f}".format(Y_scaler.scale_[0], Y_scaler.min_[0]))
+
